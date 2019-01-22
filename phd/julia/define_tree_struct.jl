6f8c2355270912be2496b3d6fb2b710ab6c86022
Suzie Brown <suzie.brown42@gmail.com>
2019-01-22 10:39:13 +0000

    define tree structure & run a few tests

---
+++ phd/julia/define_tree_struct.jl
@@ -0,0 +1,20 @@
+import AbstractTrees: children, printnode, print_tree, Leaves
+
+# define data structure for trees:
+struct TreeNode
+    generation::Int64
+    index::Int64
+    children::Vector{TreeNode3}
+end
+
+# make a tree:
+node21 = TreeNode(2,1, TreeNode[])
+node11 = TreeNode(1,1, [node21])
+
+# method of AbstractTrees::children for type TreeNode:
+children(node::TreeNode) = something(node.children, [])
+children(node11)
+
+# method of AbstractTrees::printnode for type TreeNode, prints gen.index:
+printnode(io::IO, node::TreeNode) = print(io, node.generation, '.', node.index)
+print_tree(node11)
