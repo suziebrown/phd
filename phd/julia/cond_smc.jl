5382ed182fc5aecaa92b26cf60cf1bed7b1b7427
Suzie Brown <suzie.brown42@gmail.com>
2019-01-23 09:51:13 +0000

    removed argument for immortal indices

--- phd/julia/cond_smc.jl
+++ phd/julia/cond_smc.jl
@@ -1,3 +1,3 @@
-function csmc(N::Int64, T::Int64, initialsam::Function, transition::Function, potential::Function, imortal_positions::Array{Float64,1}, immortal_indices::Array{Int64,1})
+function csmc(N::Int64, T::Int64, initialsam::Function, transition::Function, potential::Function, imortal_positions::Array{Float64,1})
     
 end
