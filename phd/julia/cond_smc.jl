e4531d566feb14ef87296fd395e9ca5f65d5b6d6
Suzie Brown <suzie.brown42@gmail.com>
2019-01-23 13:53:41 +0000

    wrote next part of csmc fn

--- phd/julia/cond_smc.jl
+++ phd/julia/cond_smc.jl
@@ -1,15 +1,29 @@
+# requires Random, Distributions, StatsBase
+
 function csmc(N::Int64, T::Int64, observations::Array{Float64,1} initialsam::Function, transition::Function, potential::Function, immortal_positions::Array{Float64,1})
     # pre-allocate memory
-    immortal_indices = Array{Int64, 1}(undef, T+1)
-    positions_old = Array{Float64, 1}(undef, N) # may not be necessary to store these
-    positions_new = Array{Float64, 1}(undef, N) # probably doesn't need initialising
+    immortal_indices = Array{Int64, 1}(undef, T+1) # probably doesn't need initialising
+    positions = Array{Float64, 1}(undef, N)  # probably doesn't need initialising
     # (also need to store ancestry tree but haven't worked out an efficient way to update it yet.)
+    weights = Array{Float64,1}(undef, N)
 
     # set OU process parameters
+    # should find a more flexible way to write this function, really for any model...
     delta = 1.0
     sigma = 0.1
 
     # initialise
-    positions_old = initialsam(N, delta, sigma)
+    positions = initialsam(N, delta, sigma)
     immortal_indices = rand(1:N, T+1) # pre-sample all of them to speed up computation
+    positions[immortal_indices[1]] = immortal_positions[1]
+    for i in 1:N
+        weights[i] = potential(positions[i], observations[1], delta, sigma) # VECTORISE ME
+    end
+    weights = weights./sum(weights) # normalise weights
+
+    for t in 0:T-1 #from here still in draft...
+        parents = sample(1:N, Weights(weights), N)
+        parents[immortal_indices[t+1]] = immortal_indices[t]
+
+    end
 end
