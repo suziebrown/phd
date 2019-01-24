4c869dd100e435d2b036841cbcfd3b284d3e97fb
Suzie Brown <suzie.brown42@gmail.com>
2019-01-24 15:53:03 +0000

    test for the 'full storage' algm

---
+++ phd/julia/test_area.jl
@@ -0,0 +1,13 @@
+# TEST AREA
+#
+using StatsBase, Random, Distributions
+
+delta = 1.0
+sigma = 0.1
+T = 10
+N = 5
+
+testobs = ousim(T, delta, sigma, false)
+immpos = ousim(T, delta, sigma, false)
+
+testres = csmc_fullstore(N, T, testobs, ouinit, outransition, oupotential, immpos)
