1b7049fe38987e90a7d96be61d2d19df0f271ef7
Suzie Brown <suzie.brown42@gmail.com>
2019-01-30 12:17:14 +0000

    drafted ourts, not effcient yet

--- phd/julia/kalman_rts.jl
+++ phd/julia/kalman_rts.jl
@@ -1,4 +1,5 @@
-# kalman filter & rts smoother
+# Kalman filter & RTS smoother for OU model
+#
 
 function oukalman(delta::Float64, sigma::Float64, observations::Array{Float64,1})
     T = length(observations) - 1
@@ -11,7 +12,7 @@ function oukalman(delta::Float64, sigma::Float64, observations::Array{Float64,1}
     tempx = 0
     tempsigma = 1
 
-    # recursion
+    # forward recursion
     for t in 1:T
         a = tempsigma / (tempsigma + sigma^2)
         xhat[t] = tempx + a * (observations[t] - tempx)
@@ -28,7 +29,28 @@ function oukalman(delta::Float64, sigma::Float64, observations::Array{Float64,1}
     return (mean = xhat, variance = sigma)
 end
 
+# could be useful to have a different version that takes kalman output as input...
 function ourts(delta::Float64, sigma::Float64, observations::Array{Float64,1})
-    # call to oukalman in here
-    #NOT FINISHED
+    # forward Kalman pass
+    kalman = oukalman(delta, sigma, observations)
+    kalmean = kalman.mean
+    kalvar = kalman.variance
+
+    # assign memory - ACTUALLY, we could overwrite kalmean/kalvar instead of saving both...
+    xhat = Array{Float64, 1}(undef, T+1) # means \hat{x}_k
+    sigma = Array{Float64,1}(undef, T+1) # variances \Sigma_k
+
+    # initialise
+    xhat[T+1] = kalmean[T+1]
+    sigma[T+1] = kalvar[T+1]
+
+    # backward recursion
+    for i in 1:T
+        t = T+1 - i # go backwards in time
+        a = (1-delta) * kalvar[t] / ((1-delta)^2 * kalvar[t] + delta)
+        xhat[t] = a * (xhat[t+1] - (1-delta) * kalmean[t])
+        sigma[t] = a * (a * sigma[t+1] - (1-delta) * kalvar[t])
+    end
+
+    return (mean = xhat, variance = sigma)
 end
