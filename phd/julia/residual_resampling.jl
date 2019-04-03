6ca4aaee1694a021f9b8a55bccb49f26fa811e5b
Suzie Brown <suzie.brown42@gmail.com>
2019-04-03 14:32:02 +0100

    also added comparison of MAD from expected offspring numbers

--- phd/julia/residual_resampling.jl
+++ phd/julia/residual_resampling.jl
@@ -17,10 +17,13 @@ end
 v_mn = Array{Int64, 2}(undef, reps, n)
 v_res = Array{Int64, 2}(undef, reps, n)
 n_random = Array{Int64, 1}(undef, reps)
+mad_mn = Array{Float64, 1}(undef, reps)
+mad_res = Array{Float64, 1}(undef, reps)
 
 # simulate resampling with multinomial & residual
 for i in 1:reps
     v_mn[i,:] = rand(Multinomial(n, w[i,:])) # multinomial resampling
+    mad_mn[i] = mean(abs.(v_mn[i,:] .- n * w[i,:])) # mean absolute deviation from mean offspring numbers
 
     # residual resmapling procedure...
     v_res_det = floor.(Int, n * w[i,:]) # deterministic part
@@ -32,6 +35,7 @@ for i in 1:reps
     v_res_rand = rand(Multinomial(R, resid)) # random part
 
     v_res[i,:] = v_res_det + v_res_rand # residual resampling
+    mad_res[i] = mean(abs.(v_res[i,:] .- n * w[i,:]))
 end
 
 # compare offspring variance etc...
@@ -52,9 +56,11 @@ end
 n_greater = sum(pairs_res .> pairs_mn)
 n_equal = sum(pairs_res .== pairs_mn)
 avg_random = mean(n_random)
+mad_greater = sum(mad_res .> mad_mn)
 
 println()
+println("On average, residual assigned ", avg_random, " out of ", n, " offspring randomly.")
 println("Residual merged more pairs than multinomial in ", n_greater, " out of ", reps, " cases.")
 println("Residual merged the same number of pairs as multinomial in ", n_equal, " out of ", reps, " cases.")
-println("On average, residual assigned ", avg_random, " out of ", n, " offspring randomly.")
+println("Residual gave higher MAD from expected offspring numbers in ", mad_greater, " out of ", reps, " cases.")
 println()
