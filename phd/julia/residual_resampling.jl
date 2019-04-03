de14ca5abc3642b75754263d482e892c4090157b
Suzie Brown <suzie.brown42@gmail.com>
2019-04-03 14:01:33 +0100

    added comparison of number of pair mergers in each sample

--- phd/julia/residual_resampling.jl
+++ phd/julia/residual_resampling.jl
@@ -17,19 +17,13 @@ end
 v_mn = Array{Int64, 2}(undef, reps, n)
 v_res = Array{Int64, 2}(undef, reps, n)
 
-v_res_det = Array{Int64, 1}(undef, n)
-v_res_rand = Array{Int64, 1}(undef, n)
-resid = Array{Float64, 1}(undef, n)
-
+# simulate resampling with multinomial & residual
 for i in 1:reps
     v_mn[i,:] = rand(Multinomial(n, w[i,:])) # multinomial resampling
 
+    # residual resmapling procedure...
     v_res_det = floor.(Int, n * w[i,:]) # deterministic part
     resid = w[i,:] .- (v_res_det /n) # residual weights
-    # for j in 1:n
-    #     v_res_det[j] = floor(Int, n * w[i,j]) # deterministic part
-    #     resid[j] = w[i,j] - (v_res_det[j] /n) # residual weights
-    # end
     R = sum(resid)
     resid = resid / R
     R = Int(round(n * R))
@@ -37,3 +31,24 @@ for i in 1:reps
 
     v_res[i,:] = v_res_det + v_res_rand # residual resampling
 end
+
+# compare offspring variance etc...
+pairs_mn = Array{Int64, 1}(undef, reps)
+binom_mn = Array{Int64,1}(undef, n)
+pairs_res = Array{Int64, 1}(undef, reps)
+binom_res = Array{Int64,1}(undef, n)
+
+for i in 1:reps
+    for j in 1:n
+        binom_mn[j] = binomial(v_mn[i,j],2)
+        binom_res[j] = binomial(v_res[i,j],2)
+    end
+    pairs_mn[i] = sum(binom_mn)
+    pairs_res[i] = sum(binom_res)
+end
+
+n_greater = sum(pairs_res .> pairs_mn)
+n_equal = sum(pairs_res .== pairs_mn)
+
+println("residual merged more pairs in ", n_greater, " out of ", reps, " cases.")
+println("residual merged the same number of pairs in ", n_equal, " out of ", reps, " cases.")
