3e72d1191a554abb99fa6b25d97dd034706d85fd
Suzie Brown <suzie.brown42@gmail.com>
2020-09-07 12:00:54 +0100

    filing

--- phd/julia/Plots/compare_resampling/resample_probs_N3.jl
+++ phd/julia/Plots/compare_resampling/resample_probs_N3.jl
@@ -34,6 +34,22 @@ function prob111res(x, y) # set input types
     #return((sum(nubar .< 0, dims=2) .== 0) .* factorial.(R) .* prod(wbar .^ nubar, dims=2))
 end
 
+function probres(x, y, nu) # set input types
+    N = 3
+    out = Array{Float64, 1}(undef, length(x))
+    w = [x y (1 .-x .-y)]
+    flnw = floor.(N * w)
+    nubar = nu .- flnw
+    wbar = N * w .- flnw
+    R = convert.(Int64, round.(sum(wbar,dims=2)))
+    for i in 1:length(x)
+        wbar[i,:] = (R[i]>0) ? (wbar[i,:] / R[i]) : (wbar[i,:] * 0)
+        out[i] = (sum(nubar[i,:] .< 0) == 0) ? factorial(R[i]) * prod(wbar[i,:] .^ nubar[i,:]) : 0
+    end
+    return(out)
+    #return((sum(nubar .< 0, dims=2) .== 0) .* factorial.(R) .* prod(wbar .^ nubar, dims=2))
+end
+
 using Plots
 
 ternaryheatmap(prob111mn, 4)
