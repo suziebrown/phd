9377b7451c18a822c1a2d79dd09a73a5598e4892
Suzie Brown <suzie.brown42@gmail.com>
2020-12-18 09:23:15 +0000

    half started an implementation of different resampling schemes

---
+++ phd/julia/resampling/resampling.jl
@@ -0,0 +1,34 @@
+### Implementation of different resampling schemes
+
+using Distributions
+
+N=4
+
+# Randomly generate the vector of weights
+function getw(N)
+    w = rand(Float64, N)
+    sumw = sum(w)
+    w/sumw
+end
+
+
+# Multinomial resampling
+function resam_mn(N,w)
+    rand(Multinomial(N,w))
+end
+
+
+# Residual-multinomial resampling
+function resam_resmn(N,w)
+    floors = Int.(floor.(N.*w))
+    resids = (N.*w - floors)
+    R = sum(resids)
+    resids = resids/R
+    floors + rand(Multinomial(Int(R),resids))
+end
+
+
+# Stratified resampling
+
+
+# Systematic resampling
