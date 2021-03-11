c3698c0e2f23ea84231fe7bb9f7d2a676dbaec1c
Suzie Brown <suzie.brown42@gmail.com>
2021-03-11 15:59:11 +0000

    plot tau function for same run with different resampling schemes

--- phd/julia/intro_plots/timescale/plot_tauN.jl
+++ phd/julia/intro_plots/timescale/plot_tauN.jl
@@ -27,7 +27,7 @@ function smc_example(N::Int64, T::Int64, observations::Array{Float64,1}, initial
 
     for t in 1:T # note: index t+1 corresponds to generation t
         # choose parents
-        parents[t, :] = sample(1:N, Weights(weights[t,:]), N)
+        parents[t, :] = resam_mn(N, weights[t,:])
         # calculate offspring counts
         for i in 1:N
             offsprcounts[i] = count(j->(j==i), parents[t, :])
@@ -44,20 +44,190 @@ function smc_example(N::Int64, T::Int64, observations::Array{Float64,1}, initial
     return cN
 end
 
+# Now a memory-efficient implementation that doesn't save stuff we don't care about
+function smc_example_fast(N::Int64, T::Int64, observations::Array{Float64,1}, initialsam::Function, transition::Function, potential::Function)
+    # pre-allocate memory
+    parents = Array{Int64, 1}(undef, N)
+    cN = Array{Float64,1}(undef, T) # for output
+    positions = Array{Float64, 1}(undef, N)
+    weights = Array{Float64, 1}(undef, N)
+    offsprcounts = Array{Int64,1}(undef, N) # for temporary storage within loop
+
+    # initialise
+    positions = initialsam(N, delta, sigma)
+    weights = potential(positions, observations[1], delta, sigma)
+    weights = weights / sum(weights) # normalise weights
+
+    # pre-calculate random numbers for OU transitions
+    rands = reshape(rand(Normal(), N*T), T, N)
+
+    for t in 1:T # note: index t+1 corresponds to generation t
+        # choose parents
+        parents = resam_mn(N, weights)
+        # calculate offspring counts
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents)
+        end
+        # calculate cN
+        cN[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        # update positions
+        positions = outransition(positions[parents], delta, sigma, rands[t, :])
+        # compute weights
+        weights = potential(positions, observations[t+1], delta, sigma)
+        weights = weights / sum(weights)
+    end
+
+    return cN
+end
+
+# Now with various different resampling schemes, but the same transition rands
+function smc_example_compare(N::Int64, T::Int64, observations::Array{Float64,1}, initialsam::Function, transition::Function, potential::Function)
+    # pre-allocate memory
+    parents_mn = Array{Int64, 1}(undef, N)
+    cN_mn = Array{Float64,1}(undef, T) # for output
+    positions_mn = Array{Float64, 1}(undef, N)
+    weights_mn = Array{Float64, 1}(undef, N)
+    parents_strat = Array{Int64, 1}(undef, N)
+    cN_strat = Array{Float64,1}(undef, T) # for output
+    positions_strat = Array{Float64, 1}(undef, N)
+    weights_strat = Array{Float64, 1}(undef, N)
+    parents_syst = Array{Int64, 1}(undef, N)
+    cN_syst = Array{Float64,1}(undef, T) # for output
+    positions_syst = Array{Float64, 1}(undef, N)
+    weights_syst = Array{Float64, 1}(undef, N)
+    parents_resmn = Array{Int64, 1}(undef, N)
+    cN_resmn = Array{Float64,1}(undef, T) # for output
+    positions_resmn = Array{Float64, 1}(undef, N)
+    weights_resmn = Array{Float64, 1}(undef, N)
+    parents_resstrat = Array{Int64, 1}(undef, N)
+    cN_resstrat = Array{Float64,1}(undef, T) # for output
+    positions_resstrat = Array{Float64, 1}(undef, N)
+    weights_resstrat = Array{Float64, 1}(undef, N)
+    parents_ressyst = Array{Int64, 1}(undef, N)
+    cN_ressyst = Array{Float64,1}(undef, T) # for output
+    positions_ressyst = Array{Float64, 1}(undef, N)
+    weights_ressyst = Array{Float64, 1}(undef, N)
+    offsprcounts = Array{Int64,1}(undef, N) # for temporary storage within loop
+
+    # initialise
+    positions_mn = initialsam(N, delta, sigma)
+    weights_mn = potential(positions_mn, observations[1], delta, sigma)
+    weights_mn = weights_mn / sum(weights_mn) # normalise weights
+    positions_strat = initialsam(N, delta, sigma)
+    weights_strat = potential(positions_strat, observations[1], delta, sigma)
+    weights_strat = weights_strat / sum(weights_strat)
+    positions_syst = initialsam(N, delta, sigma)
+    weights_syst = potential(positions_syst, observations[1], delta, sigma)
+    weights_syst = weights_syst / sum(weights_syst)
+    positions_resmn = initialsam(N, delta, sigma)
+    weights_resmn = potential(positions_resmn, observations[1], delta, sigma)
+    weights_resmn = weights_resmn / sum(weights_resmn)
+    positions_resstrat = initialsam(N, delta, sigma)
+    weights_resstrat = potential(positions_resstrat, observations[1], delta, sigma)
+    weights_resstrat = weights_resstrat / sum(weights_resstrat)
+    positions_ressyst = initialsam(N, delta, sigma)
+    weights_ressyst = potential(positions_ressyst, observations[1], delta, sigma)
+    weights_ressyst = weights_ressyst / sum(weights_ressyst)
+
+    # pre-calculate random numbers for OU transitions
+    rands = reshape(rand(Normal(), N*T), T, N)
+
+    for t in 1:T # note: index t+1 corresponds to generation t
+        # choose parents
+        parents_mn = resam_mn(N, weights_mn)
+        # calculate offspring counts
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_mn)
+        end
+        # calculate cN
+        cN_mn[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        # update positions
+        positions_mn = outransition(positions_mn[parents_mn], delta, sigma, rands[t, :])
+        # compute weights
+        weights_mn = potential(positions_mn, observations[t+1], delta, sigma)
+        weights_mn = weights_mn / sum(weights_mn)
+
+        parents_strat = resam_strat(N, weights_strat)
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_strat)
+        end
+        cN_strat[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        positions_strat = outransition(positions_strat[parents_strat], delta, sigma, rands[t, :])
+        weights_strat = potential(positions_strat, observations[t+1], delta, sigma)
+        weights_strat = weights_strat / sum(weights_strat)
+
+        parents_syst = resam_syst(N, weights_syst)
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_syst)
+        end
+        cN_syst[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        positions_syst = outransition(positions_syst[parents_syst], delta, sigma, rands[t, :])
+        weights_syst = potential(positions_syst, observations[t+1], delta, sigma)
+        weights_syst = weights_syst / sum(weights_syst)
+
+        parents_resmn = resam_resmn(N, weights_resmn)
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_resmn)
+        end
+        cN_resmn[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        positions_resmn = outransition(positions_resmn[parents_resmn], delta, sigma, rands[t, :])
+        weights_resmn = potential(positions_resmn, observations[t+1], delta, sigma)
+        weights_resmn = weights_resmn / sum(weights_resmn)
+
+        parents_resstrat = resam_resstrat(N, weights_resstrat)
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_resstrat)
+        end
+        cN_resstrat[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        positions_resstrat = outransition(positions_resstrat[parents_resstrat], delta, sigma, rands[t, :])
+        weights_resstrat = potential(positions_resstrat, observations[t+1], delta, sigma)
+        weights_resstrat = weights_resstrat / sum(weights_resstrat)
+
+        parents_ressyst = resam_ressyst(N, weights_ressyst)
+        for i in 1:N
+            offsprcounts[i] = count(j->(j==i), parents_ressyst)
+        end
+        cN_ressyst[t] = sum(offsprcounts .* (offsprcounts .- 1))./(N*(N-1))
+        positions_ressyst = outransition(positions_ressyst[parents_ressyst], delta, sigma, rands[t, :])
+        weights_ressyst = potential(positions_ressyst, observations[t+1], delta, sigma)
+        weights_ressyst = weights_ressyst / sum(weights_ressyst)
+    end
+
+    return (cN_mn=cN_mn, cN_strat=cN_strat, cN_syst=cN_syst, cN_resmn=cN_resmn, cN_resstrat=cN_resstrat, cN_ressyst=cN_ressyst)
+end
+
 ### Let's go!
 
-T=10 #time horizon
-N=20 # number of particles
+T=150 #time horizon
+N=150 # number of particles
 # OU process parameters:
-delta = 1.0 # step size & transition noise (variance)
-sigma = 0.1 # observation noise (s.d.)
+delta = 0.1 # step size & transition noise (variance)
+sigma = 1.0 # observation noise (s.d.)
 # generate realisation
 mysimobs = ousim(T, delta, sigma, false)
-mysimcN = smc_example(N, T, mysimobs, ouinit, outransition, oupotential)
-sumcN = cumsum(mysimcN)
+#mysimcN = smc_example(N, T, mysimobs, ouinit, outransition, oupotential)
+#mysimcN = smc_example_fast(N, T, mysimobs, ouinit, outransition, oupotential)
+#sumcN = cumsum(mysimcN)
 
 # PLOTS
 # plot observations:
 scatter(0:T, mysimobs, marker=(:circle, 0.6, :black), leg=false)
 # plot tau_N:
-plot([0;sumcN], 0:T, line=(:steppre, 0.6, :black), leg=false, xaxis="t", yaxis="tau_N(t)")
+#plot([0;sumcN], 0:T, line=(:steppre, 0.6, :black), leg=false, xaxis="t", yaxis="tau_N(t)")
+
+# Comparison
+mysimcNs = smc_example_compare(N, T, mysimobs, ouinit, outransition, oupotential)
+sumcN_mn = cumsum(mysimcNs.cN_mn)
+sumcN_strat = cumsum(mysimcNs.cN_strat)
+sumcN_syst = cumsum(mysimcNs.cN_syst)
+sumcN_resmn = cumsum(mysimcNs.cN_resmn)
+sumcN_resstrat = cumsum(mysimcNs.cN_resstrat)
+sumcN_ressyst = cumsum(mysimcNs.cN_ressyst)
+
+plot([0;sumcN_mn], 0:T, line=(:steppre, 0.6, :black), lab="multinomial", legend=:bottomright, xaxis="t", yaxis="tau_N(t)")
+plot!([0;sumcN_strat], 0:T, line=(:steppre, 0.6, :green), lab="stratified")
+plot!([0;sumcN_syst], 0:T, line=(:steppre, 0.6, :blue), lab="systematic")
+plot!([0;sumcN_resmn], 0:T, line=(:steppre, 0.6, :red), lab="residual-mn")
+plot!([0;sumcN_resstrat], 0:T, line=(:steppre, 0.6, :purple), lab="residual-strat")
+plot!([0;sumcN_ressyst], 0:T, line=(:steppre, 0.6, :orange), lab="residual-syst")
+title!("T = $(T), N = $(N), delta = $(delta), sigma = $(sigma)")
